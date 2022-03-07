e8506982c640b49aec22bbf5b1c22ad50b290740
Maksymilian Wolański <91730940+maksymilian-wolanski@users.noreply.github.com>
2022-03-07 17:58:09 +0100

    Add files via upload

---
+++ 1.2.php
@@ -0,0 +1,30 @@
+<?php
+
+$dzien = 1;
+
+switch ($dzien){
+    case 1:
+        echo "Poniedziałek";
+        break;
+    case 2:
+        echo "Wtorek";
+        break;
+    case 3:
+        echo "Środa";
+        break;
+    case 4:
+        echo "Czwartek";
+        break;
+    case 5:
+        echo "Piątek";
+        break;
+    case 6:
+        echo "Sobota";
+        break;
+    case 7:
+        echo "Niedziela";
+        break;
+    default:
+        echo "Niepoprawny numer.";
+        break;
+}
