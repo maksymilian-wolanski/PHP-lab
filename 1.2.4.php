82686c39ac2c629ead40c592f0fe67695e9cb54f
Maksymilian Wolański <91730940+maksymilian-wolanski@users.noreply.github.com>
2022-03-28 09:31:51 +0200

    Update 1.2.4.php

--- 1.2.4.php
+++ 1.2.4.php
@@ -1,10 +1,33 @@
 <?php
 	function datazpesel($pesel){
 	  $tab = array();
-	  for(int i = 0; i < 11; i++){
-      array_push($tab, $pesel[i]);
+	  $i = 0;
+	  while ($i <11){
+      array_push($tab, $pesel[$i]);
+      $i += 1;
 	  }
-	  var_dump($tab);
+	  if ($tab[2] <= 1){
+	    echo ($tab[4]);
+	    echo ($tab[5]);
+	    echo ("-");
+	    echo ($tab[2]);
+	    echo ($tab[3]);
+	    echo ("-");
+	    echo ("20");
+	    echo ($tab[0]);
+	    echo ($tab[1]);
+	  }
+	  else{
+	    echo ($tab[4]);
+	    echo ($tab[5]);
+	    echo ("-");
+	    echo ($tab[2] - 2);
+	    echo ($tab[3]);
+	    echo ("-");
+	    echo ("19");
+	    echo ($tab[0]);
+	    echo ($tab[1]);
+	  }
 	}
 	datazpesel("02070803628")
 ?>
